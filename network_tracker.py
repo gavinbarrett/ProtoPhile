30b6700d45ccf0f3fb1772d6c097f698d4bb63a9
Gavin <gavinbrrtt@gmail.com>
2020-08-21 13:16:51 -0700

    Added support for UDP analyzing. Added colors and text formatting.

--- network_tracker.py
+++ network_tracker.py
@@ -1,10 +1,15 @@
+import tty
 import struct
 import socket
+import termios
 
 # create socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-s2 = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+s2 = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
 
+GREEN = '\u001b[32m'
+WHITE = '\u001b[37m'
+CYAN = '\u001b[36m'
 def handle_tcp():
 	print("Encountered TCP packet")
 
@@ -14,10 +19,26 @@ def handle_udp():
 def unpack_ip(ip_header):
 	return struct.unpack("!BBHHHBBH4s4s", ip_header)
 
-i = 0
-for i in range(0, 20):
+def get_packet_type(pack_type):
+	if pack_type == 6:
+		return 'TCP'
+	elif pack_type == 17:
+		return 'UDP'
+	else:
+		print(f'Unknown packet type: {pack_type}')
+		return 'Unknown'
+
+def host_lookup(addr):
+	try:
+		# try to retrieve the host name
+		host = socket.gethostbyaddr(addr)
+		return host[0]
+	except:
+		return addr
+
+def read_socket(sock, i):
 	# receive tuple of data and ...
-	packet = s.recvfrom(65536)
+	packet = sock.recvfrom(65536)
 	# grab network data
 	packet = packet[0]
 	# strip out ip header
@@ -26,22 +47,33 @@ for i in range(0, 20):
 	#print(f"ip_head: {ip_head}\n")
 	# unpack bytes of ip_header
 	iph = unpack_ip(ip_head)
-	if iph[6] == 6:
-		handle_tcp()
-	elif iph[6] == 17:
-		handle_udp()
+	#if iph[6] == 6:
+	#	handle_tcp()
+	#elif iph[6] == 17:
+	#	handle_udp()
+	packet_type = get_packet_type(iph[6])
+
 	x = (iph[0] >> 4) & 0x0F
-	print(f"Version: {x}")
-	print(f"Total Length: {iph[2]}")
-	print(f"Protocol: {iph[6]}")
-	print(f"Header checksum: {iph[7]}")
+	#print(f"Version: {x}")
+	#print(f"Total Length: {iph[2]}")
+	#print(f"Protocol: {iph[6]}")
+	#print(f"Header checksum: {iph[7]}")
 	# print source and destination ips
 	src = socket.inet_ntoa(iph[8])
-	try:
-		src = socket.gethostbyaddr(src)[0]
-	except:
-		print('src hostname could not be found')
-	print(src)
-	print(f"Source:	  {socket.inet_ntoa(iph[8])}")
-	print(f"Destination: {socket.inet_ntoa(iph[9])}")
-	print(f"Packet Data:\n{data}\n")
+	# retrieve the src and dest hostnames
+	src_host = host_lookup(socket.inet_ntoa(iph[8]))
+	dest_host = host_lookup(socket.inet_ntoa(iph[9]))
+	
+	if packet_type == 'TCP':
+		COLOR = GREEN
+	else:
+		COLOR = CYAN
+	print(f"{WHITE} #{i}: {COLOR}{packet_type}{WHITE} - {src_host} \u2192 {dest_host}")
+
+i = 0
+while True:
+	read_socket(s, i)
+	i += 1
+	read_socket(s2, i)
+	i += 1	
+	#print(f"Packet Data:\n{data}\n")
